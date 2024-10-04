fdb578365868d983e2d55720a3a06bd8cf236a64
mwihoti <danielmwihoti@gmail.com>
2024-10-04 18:36:47 +0300

    User api route

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import {prisma, getDataWithCache} from "../../../lib/database";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+        return NextResponse.json({error: "User ID is required"}, {status: 400});
+
+    }
+    try {   
+        const user = await getDataWithCache(
+            `user:${userId}`,
+            () => prisma.user.findUnique({
+                where: { id: parseInt(userId) },
+                include: { enrolledClasses: true},
+            })
+        );
+        if (!user) {
+            return NextResponse.json({error: "User not found"}, {status: 404});
+        }
+        return NextResponse.json({ user});
+
+    } catch (error) {
+        console.error("Error: ", error);
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500});
+    }
+}
