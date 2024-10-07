1514a301d2f7a101ee71a65bac8d9b40db8dfa73
mwihoti <danielmwihoti@gmail.com>
2024-10-07 16:53:42 +0300

    Booking api routes

--- src/app/trainer/page.tsx
+++ src/app/trainer/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import TrainerShowcasePage from '@/components/Trainers'
 const page: React.FC = () => {
   return (
-    <div><h2>Trainers page</h2></div>
+    <div><h2>Trainers page</h2>
+    <TrainerShowcasePage />
+    </div>
   )
 }
 
