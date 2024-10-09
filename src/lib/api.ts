c9ec7a2efcbb9f6e0e7734d3f2739d9a30f03d11
mwihoti <danielmwihoti@gmail.com>
2024-10-09 10:28:31 +0300

    create a new file for api calls

---
+++ src/lib/api.ts
@@ -0,0 +1,36 @@
+const BASE_URL = '/api/book';
+
+export const fetchBookings = async (clientId?: string, trainerId?: string) => {
+    const url = new URL(BASE_URL, window.location.origin);
+    if (clientId) url.searchParams.append('clientId', clientId);
+    if (trainerId) url.searchParams.append('trainerId', trainerId);
+
+    const response = await fetch(url.toString());
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch bookings');
+    }
+
+    return response.json();
+};
+
+export const creatingBooking = async (bookingData: {
+    clientId: number;
+    trainerId: number;
+    className: string;
+    dateTime: string;
+    note?: string;
+    sessionType: string;
+}) => {
+    const response = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookingData),
+    });
+    if (!response.ok) {
+        throw new Error('Failed to create booking')
+    }
+    return response.json();
+}
