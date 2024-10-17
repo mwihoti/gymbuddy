fd6b2bde473bc79f9aa9fccd798174a57105cf0b
mwihoti <danielmwihoti@gmail.com>
2024-10-17 20:03:22 +0300

    trainer booking route

--- src/app/api/trainers/[trainerId]/bookings/route.ts
+++ src/app/api/trainers/[trainerId]/bookings/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function Get(
+export async function GET(
     request: Request,
     { params }: { params: { trainerId: string }}
 ) {
