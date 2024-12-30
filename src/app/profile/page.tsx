54eb05fc1cc9059eed2a728588df7d5cf6bca712
mwihoti <danielmwihoti@gmail.com>
2024-12-30 21:28:20 +0300

    feat add useswr hook

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,18 @@
+'use client'
 import React, {useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import useSWR from 'swr';
 import Cookies from 'js-cookie';
 
+const  fetcher = (...args: [RequestInfo, RequestInit?]) => fetch(...args).then(res => res.json() )
+
 
 const Profile: React.FC = () => {
+    const router = useRouter();
+    const { data, error } = useSWR('/api/users', fetcher);
+
+    const [userData, setUserData] = useState(null);
+
 
     return(
         <div>
