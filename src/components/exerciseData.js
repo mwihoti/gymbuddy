d583c160c6b00b04aaf10daeb7c29f93434706ad
mwihoti <danielmwihoti@gmail.com>
2025-08-04 19:27:02 +0300

    add get exerciseData function

--- src/components/exerciseData.js
+++ src/components/exerciseData.js
@@ -85,8 +85,175 @@ export const exerciseData = {
     "Lat Pulldown": {
         image: "/assets/exercises/latpulldon.png",
         description: "Machine exercise targeting latissimus dorsi",
-        
+        difficulty: "Beginner",
+        equipment: "Machine"
+    },
+    "Deadlifts": {
+        image: "/assets/exercises/deadlifts.png",
+        description: "Full-body compound movement for strength training, targeting the legs, back and core",
+        difficulty: "Intermediate",
+        equipment: "Barbell"
+    },
+    "Face Pulls": {
+        image: "/assets/exercises/facepulls.png",
+        description: "Training exercise that primarily works the muscles in the upper back and shoulders",
+        difficulty: "Beginner",
+        equipment: "Cable"
+    },
+    "Supermans": {
+        image: "/assets/exercises/supermans.png",
+        description: "BodyWeight exercise that primarily targets the muscles in your back, glutes and core",
+        difficulty: "Beginner",
+        equipment: "Bodyweiight"
+    },
+    "Back Extensions": {
+        image: "/assets/exercises/backextensions.png",
+        description: "Lower back strengthening exercise",
+        difficulty: "Beginner",
+        equipment: "Bodyweight"
+    },
+    "Reverse Flys": {
+        image: "/assets/exercises/reversefly.png",
+        description: "Rear deltoid isolation exercise",
+        difficulty: "Beginner",
+        equipment: "Dumbbell"
+    },
 
-    }
+    // Shoulder exercise
 
+    "Overhead Press": {
+        image: "/assets/exercises/overheadpress.png",
+        description: "Compound shoulder exercise, targets shoulder muscles and  triceps, while also engaging core for stability",
+        difficulty: "Intermediate",
+        equipment: "Barbell, Dumbbells, kettlebells"
+    },
+    "lateral Raises": {
+        image: "/assets/exercises/lateralRaises.png",
+        description: "Shoulder Isolation exercise targeting lateral deltoids, the muscles on the sides of your shoulders.",
+        difficulty: "Beginner",
+        equipment: "Dumbbell, kettleBells"
+    },
+    "Arnold press": {
+        image: "/assets/exercises/arnoldpress.png",
+        description: "Rotational shoulder press variation, characterized by a rotational movement of the wrist during the lift.",
+        difficulty: "Intermediate",
+        equipment: "Dumbbell"
+    },
+
+
+    // Triceps exercise
+    "Tricep Dips": {
+        image: "/assets/exercises/tricepdips.png",
+        description: "bodyweight exercise that primarily works the triceps muscles, located on the back of the upper arm.",
+        difficulty: "Intermediate",
+        equipment: "Bodyweight"
+    },
+    "Tricep Pushdowns": {
+        image: "/assets/exercises/triceppushdown.png",
+        description: "cable isolation exercise targeting the triceps muscle group.",
+        difficulty: "Beginner",
+        equipment: "Cable"
+    },
+    "Skull crushers": {
+        image: "/assets/exercises/skullcrusher.png",
+        description: "Also known as lying triceps extensions, strength training exercise that primarily targets the triceps muscles in the back of the upper arm",
+        difficulty: "Intermediate",
+        equipment: "Dumbbell, Barbell"
+    },
+
+    // core exercise
+    "Russian Twists": {
+        image: "/assets/exercises/russianTwists.png",
+        description: "Rotational core exercise",
+        difficulty: "Beginner",
+        equipment: "BodyWeight"
+    },
+    "Side Planks": {
+        image: "/assets/exercises/sideplanks.png",
+        description: "Core strengthening exercise where you balance on one forearm and the side of one foot, maintaining a straight line from head to feet",
+        difficulty: "Beginner",
+        equipment: "BodyWeight"
+    },
+    "Bicylcle Crunches": {
+        image: "/assets/exercises/bicyclecrunches.png",
+        description: "Abdominal exercise that involves a twisting motion, mimicking the action of pedaling a bicycle, to engage the core muscles, particularly the obliques.",
+        difficulty: "Beginner",
+        equipment: "Bodyweight"
+    },
+
+    // Glute exercises
+    "Glute Bridges": {
+        image: "/assets/exercises/glutebridges.png",
+        description: "Bodyweight exercise where you lie on your back, knees bent, and lift your hips off the ground to create a straight line from your shoulders to your knees.",
+        difficulty: "Beginner",
+        equipment: "BodyWeight"
+    },
+    "Hip Thrusts": {
+        imsge: "/assets/exercises/hipthrusts.png",
+        description: "Strength training exercise where you lift your hips off the ground while lying on your back, typically with your upper back supported on the bench.",
+        difficulty: "Beginner",
+        equipment: "BodyWeight"
+    },
+    // Calf exercises
+    "Calf Raises": {
+        image: "/assets/exercises/calfraises.png",
+        description: "Known as heel raises, targets the calf muscles. Involves raising your heals off the ground while standing on your toes.",
+        difficulty: "Beginner",
+        equipment: "BodyWeight, Dumbbells, kettlebells, Barbell"
+    },
+    "Seated Calf Raises": {
+        image: "/assets/exercises/seatedcalfraises.png",
+        description: "Exercise that strengthens the calf muscles soleus. Involves raises the heels off the ground while keeping the knees bent and feet flat on the floor platform",
+        difficulty: "Beginner",
+        equipment: "Machine, Bodyweight, Barbell, Dumbbell"
+    },
+    "Jump Rope": {
+        image: "/assets/exercises/jumprope.png",
+        description: "Cardio exercise involves jumping a rope, targets calf muscles",
+        difficulty: "Beginner",
+        equipment: "Jump rope"
+    },
+    // Forearm exercise
+    "Wrist Curls": {
+        image: "/assets/exercises/wristcurls.png",
+        description: "Isolation exercise that focuses on strengthening the muscles of the wrist and forearms.",
+        difficulty: "Beginner",
+        equipment: "Dumbbell"
+    },
+    "Reverse Wrist Curls": {
+    image: "/assets/exercises/revrsewristcurls.png",
+    description: "Forearm extensor exercise, strengthening the muscles on the back of the forearm, wrist extensors.",
+    difficulty: "Beginner",
+    equipment: "Dumbbell"
+  },
+  "Farmer's Walk": {
+    image: "/assets/exercises/farmerswalk.png",
+    description: "Grip strength and full-body exercise, involves walking with heavy weights in each hand.",
+    difficulty: "Intermediate",
+    equipment: "Dumbbell"
+  },
+
+  // Specialty exercises
+  "Good Mornings": {
+    image: "/assets/exercises/goodmorning.png",
+    description: "Hip-hinge exercise for posterior chain including hamstrings, glutes and lower back",
+    difficulty: "Advanced",
+    equipment: "Barbell"
+  },
+  "Mountain Climbers": {
+    image: "/assets/exercises/mountainclimbers.png",
+    description: "Dynamic bodyweight movement that mimics the act of climbing, engaging multiple muscle groups simultaneously, primarily targeting the core, arms, and legs. ",
+    equipment: "Bodyweight"
+  },
+  "Leg Raises": {
+    image: "/assets/exercises/legraises.png",
+    description: "Lower abdominal exercise",
+    difficulty: "Intermediate",
+    equipment: "Bodyweight"
+  },
+
+}
+
+export const getExerciseData = (exerciseData) => {
+    return exerciseData[exercise]
 }
